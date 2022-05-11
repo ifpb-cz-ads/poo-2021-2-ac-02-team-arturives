0de5baccc1aca5b775fab3792caa594227c108f1
SabbathCode <daniel.arturjsb@gmail.com>
2022-05-11 18:10:19 -0300

    Respondendo as questoes por Daniel Artur

---
+++ CBBAtividadePratica2.java
@@ -0,0 +1,10 @@
+class CBBAtividadePratica2{
+        
+
+    public static void main(String [] args){
+        int num1 = 3;
+        int num2 = 4;
+        int num3 = 7;
+        System.out.println("A soma a dos números é: " + (num1 + num2 + num3));
+    }
+}
